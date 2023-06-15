72601c2d905d25612a5650975a0e20c024beea6d
Steve Larson <9larsons@gmail.com>
2023-06-15 09:20:48 -0500

    fix root selection handling for text or empty node

--- packages/lexical-playground/src/plugins/SelectAllPlugin/index.tsx
+++ packages/lexical-playground/src/plugins/SelectAllPlugin/index.tsx
@@ -7,7 +7,15 @@
  */
 
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import {$getRoot, COMMAND_PRIORITY_LOW, KEY_MODIFIER_COMMAND} from 'lexical';
+import {
+  $getRoot,
+  $getSelection,
+  $isDecoratorNode,
+  $isRangeSelection,
+  $isTextNode,
+  COMMAND_PRIORITY_LOW,
+  KEY_MODIFIER_COMMAND,
+} from 'lexical';
 import {useEffect} from 'react';
 
 export default function SelectAllPlugin(): null {
@@ -19,10 +27,42 @@ export default function SelectAllPlugin(): null {
       (event: KeyboardEvent) => {
         const {metaKey, code} = event;
         if (metaKey && code === 'KeyA') {
-          const root = $getRoot();
-          root.select(0, root.getChildrenSize());
-          event.preventDefault();
-          return true;
+          const selection = $getSelection();
+          if ($isRangeSelection(selection)) {
+            const root = $getRoot();
+            const firstNode = root.getFirstChildOrThrow();
+            const lastNode = root.getLastChildOrThrow();
+
+            if (firstNode && lastNode) {
+              if (!$isDecoratorNode(firstNode)) {
+                const firstChild = firstNode.getFirstChild();
+                if ($isTextNode(firstChild)) {
+                  selection.anchor.set(firstChild.getKey(), 0, 'text');
+                }
+              } else {
+                selection.anchor.set('root', 0, 'element');
+              }
+
+              if (!$isDecoratorNode(lastNode)) {
+                const lastChild = lastNode.getLastChild();
+                if ($isTextNode(lastChild)) {
+                  selection.focus.set(
+                    lastChild.getKey(),
+                    lastChild.getTextContentSize(),
+                    'text',
+                  );
+                }
+              } else {
+                selection.focus.set(
+                  'root',
+                  lastNode.getIndexWithinParent() + 1,
+                  'element',
+                );
+              }
+              event.preventDefault();
+              return true;
+            }
+          }
         }
         return false;
       },
